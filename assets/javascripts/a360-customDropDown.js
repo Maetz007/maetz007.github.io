679bada85d1683d032075bee0800bb0f91569dda
Maetz007 <maetz007@yahoo.com>
2018-02-21 10:49:17 -0600

    Adjusted aria

--- assets/javascripts/a360-customDropDown.js
+++ assets/javascripts/a360-customDropDown.js
@@ -23,6 +23,7 @@ $(document).ready(function(){
 
   $('.list_link').on('click', function(){
     var $newText = $(this).text();
+    $expElement.attr('aria-activeDescendant', $(this).attr('id'));
     $expControl.slideUp(100, 'linear');
     $('#' + $textId).siblings().removeClass('fa-angle-up').addClass('fa-angle-down');
     $($expElement).attr('aria-expanded', false);
@@ -49,11 +50,9 @@ $(document).ready(function(){
         break;
       case 38:
         $('.list_link:focus').closest('li').prev().find('.list_link').focus();
-        $expElement.attr('aria-activeDescendant', $(this).attr('id'));
         break;
       case 40:
         $('.list_link:focus').closest('li').next().find('.list_link').focus();
-        $expElement.attr('aria-activeDescendant', $(this).attr('id'));
         break;
       default: // do nothing;
     }
